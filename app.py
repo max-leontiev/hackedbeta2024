9a0fbcc98af44d550dc0b885c697846dbc89c31f
Max Leontiev <mleontie@ualberta.ca>
2024-11-09 12:04:27 -0700

    Calculation history

--- app.py
+++ app.py
@@ -3,14 +3,17 @@ from flask import Flask, request, render_template
 from markupsafe import escape
 app = Flask(__name__)
 
+calculation_history = []
+
 @app.route("/", methods=['GET', 'POST'])
 def hello():
-    return render_template("index.html")
+    return render_template("index.html", calculation_history=calculation_history)
 
 @app.route('/result', methods=['POST'])
 def read_form():
     # Get the form data as Python ImmutableDict datatype
     data = request.form
-    
+    calculation_history.append(escape(data['calculation']))
+
     ## Return the extracted information (escape it to be safe)
-    return render_template("index.html", calculation=escape(data['calculation']))
+    return render_template("index.html", calculation_history=calculation_history)
