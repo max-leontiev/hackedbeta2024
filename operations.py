b657fc5650a62dddb3b1aa9ee174dbc40cd21aa4
WillWer2134 <wbradfor@ualberta.ca>
2024-11-09 17:36:48 -0700

    added the operation classes

--- operations.py
+++ operations.py
@@ -1,6 +1,27 @@
 import numpy as np;
 from matrix import Matrix;
 
+class UOperator:
+    def __init__(self,matrix):
+        self.matrix = matrix;
+
+class BOperator:
+    def __init__(self,matrix_a,matrix_b):
+        self.matrix_a = matrix_a;
+        self.matrix_b = matrix_b;
+
+class directSum:
+    def __init__(self,matrix_a,matrix_b):
+        super().__init__(matrix_a,matrix_b);
+
+
+
+    
+    
+
+    
+
+
 def directSum(A, B): #pass in numpy arrays
     #assuming A and B are not yet numpy arrays. Comment out the following two lines after testing.
     A = np.array(A)
@@ -11,9 +32,14 @@ def directSum(A, B): #pass in numpy arrays
     result = np.zeros((dimA[0] + dimB[0], dimA[1] + dimB[1])) # creates a new array with dimensions rowsA+rowsB by columns+columnsB and populates it with zeroes
     result[:dimA[0], :dimA[1]] = A #places MatrixA in the top-left corner of result.
     result[dimA[0]:, dimA[1]:] = B # places MatrixB in the bottom-right corner of result.
-    return result
+    return result;
 
 
+
+
+def determinant(matrix):
+    A = matrix.np_array;
+    return 0;
 def inverse(matrix):  
     return 0;
 
