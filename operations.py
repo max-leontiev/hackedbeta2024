b494102818a03efee958dbe6642fc06771f8cea2
WillWer2134 <wbradfor@ualberta.ca>
2024-11-09 18:07:35 -0700

    added determinant and inverse but did not consider errors

--- operations.py
+++ operations.py
@@ -1,29 +1,43 @@
 import numpy as np;
 from matrix import Matrix;
 
+
+
+
 class UOperator:
     def __init__(self,matrix):
         self.matrix = matrix;
+    
+
+    def determinant(matrixA):
+        A = matrixA.np_array;
+        return np.linalg.det(A);
+
+    def inverse(matrixA):
+        A = matrixA.np_array;   
+        return np.linalg.inv(A);
+    
+
 
 class BOperator:
     def __init__(self,matrix_a,matrix_b):
         self.matrix_a = matrix_a;
         self.matrix_b = matrix_b;
+    
+    
+
+
+
+    
+    
 
     
 
 
-
-    
-    
-
-    
-
-
-def directSum(A, B): #pass in numpy arrays
-    #assuming A and B are not yet numpy arrays. Comment out the following two lines after testing.
-    A = np.array(A)
-    B = np.array(B)
+def directSum(matrixA, matrixB): #pass in numpy arrays
+    #assuming A and B are not yet numpy arrays. Comment out the following two lines after testing. 
+    A = matrixA.np_array;
+    B = matrixB.np_array;
     # returns the dimensions of A and B where shape is a tuple with index 0 having x rows and index 1 having y columns
     dimA = A.shape 
     dimB = B.shape
